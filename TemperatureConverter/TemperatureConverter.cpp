e0998f01bf0684f3d11e8f88cb8df97620d31fa1
luise <luiserivaldo99@gmail.com>
2022-04-07 17:58:27 +0700

    add unit symbols to output

--- TemperatureConverter/TemperatureConverter.cpp
+++ TemperatureConverter/TemperatureConverter.cpp
@@ -14,22 +14,22 @@ int main() {
 	if (userInput == 'c') {
 		cout << "Enter the temperature in Celcius: \n";
 		cin >> inputCelcius;
-		cout << "Your temperature in Fahrenheit is " << ((inputCelcius * 9 / 5) + 32) << endl;
-		cout << "Your temperature in Kelvin is " << (inputCelcius + 273.15) << endl;
+		cout << "Your temperature in Fahrenheit is " << ((inputCelcius * 9 / 5) + 32) << "\370F" << endl;
+		cout << "Your temperature in Kelvin is " << (inputCelcius + 273.15) << " K" << endl;
 	}
 
 	else if (userInput == 'f') {
 		cout << "Enter the temperature in Fahrenheit: \n";
 		cin >> inputFahrenheit;
-		cout << "Your temperature in Celcius is " << ((inputFahrenheit - 32) * 5 / 9) << endl;
-		cout << "Your temperature in Kelvin is " << ((inputFahrenheit - 32) * 5 / 9) + 273.15 << endl;
+		cout << "Your temperature in Celcius is " << ((inputFahrenheit - 32) * 5 / 9) << "\370C" << endl;
+		cout << "Your temperature in Kelvin is " << ((inputFahrenheit - 32) * 5 / 9) + 273.15 << " K" << endl;
 	}
 
 	else if (userInput == 'k') {
 		cout << "Enter the temperature in Kelvin: \n";
 		cin >> inputKelvin;
-		cout << "Your temperature in Celcius is " << (inputKelvin - 273.15) << endl;
-		cout << "Your temperature in Fahrenheit is " << ((inputKelvin - 273.15) * 9 / 5 + 32) << endl;
+		cout << "Your temperature in Celcius is " << (inputKelvin - 273.15) << "\370C" << endl;
+		cout << "Your temperature in Fahrenheit is " << ((inputKelvin - 273.15) * 9 / 5 + 32) << "\370F" << endl;
 	}
 
 	return 0;
