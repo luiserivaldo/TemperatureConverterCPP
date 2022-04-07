2dc51621ca6e9bfe456189a29e0b5c833f26c6ff
luise <luiserivaldo99@gmail.com>
2022-04-07 15:00:49 +0700

    initial commit

--- TemperatureConverter/TemperatureConverter.cpp
+++ TemperatureConverter/TemperatureConverter.cpp
@@ -7,7 +7,22 @@ int main() {
 	double farhenheit = 0.0;
 	double kelvin = 0.0;
 
+	// user input variables
+	double inputCelcius = 0.0;
+	double inputFarhenheit =0.0;
+	double inputKelvin =0.0;
+
 	char userInput;
-	cout << "Enter the temperature Unit [C, F, K]" << endl;
+	cout << "Enter the temperature Unit: [C]elcius, [F]arhenheit, [K]elvin:" << endl;
 	cin >> userInput;
+
+	if (userInput == 'c') {
+		cout << "Enter the temperature in Celcius: \n";
+		cin >> inputCelcius;
+		cout << "Your temperature in Farhenheit is " << (inputCelcius - 32) * (5.0 / 9.0) << endl;
+		cout << "Your temperature in Kelvin is " << (inputCelcius + 273.15) << endl;
+	}
+
+
+
 }
