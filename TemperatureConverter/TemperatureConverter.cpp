585119df6610746233aa27571a96e98020033af0
luise <luiserivaldo99@gmail.com>
2022-04-07 14:51:17 +0700

    Add project files.

---
+++ TemperatureConverter/TemperatureConverter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// temperature units
+	double celcius = 0.0;
+	double farhenheit = 0.0;
+	double kelvin = 0.0;
+
+	char userInput;
+	cout << "Enter the temperature Unit [C, F, K]" << endl;
+	cin >> userInput;
+}
