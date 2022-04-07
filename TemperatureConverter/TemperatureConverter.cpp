0202fdd4b4df5636a40eb716a4ffae5725f577a1
luise <luiserivaldo99@gmail.com>
2022-04-07 15:20:24 +0700

    remove redundancies and typos

--- TemperatureConverter/TemperatureConverter.cpp
+++ TemperatureConverter/TemperatureConverter.cpp
@@ -2,39 +2,34 @@
 using namespace std;
 
 int main() {
-	// temperature units
-	double celcius = 0.0;
-	double farhenheit = 0.0;
-	double kelvin = 0.0;
-
 	// user input variables
 	double inputCelcius = 0.0;
-	double inputFarhenheit =0.0;
+	double inputFahrenheit =0.0;
 	double inputKelvin =0.0;
 
 	char userInput;
-	cout << "Enter the temperature Unit: [C]elcius, [F]arhenheit, [K]elvin:" << endl;
+	cout << "Enter the temperature Unit: [C]elcius, [F]ahrenheit, [K]elvin:" << endl;
 	cin >> userInput;
 
 	if (userInput == 'c') {
 		cout << "Enter the temperature in Celcius: \n";
 		cin >> inputCelcius;
-		cout << "Your temperature in Farhenheit is " << ((inputCelcius * 9 / 5) + 32) << endl;
+		cout << "Your temperature in Fahrenheit is " << ((inputCelcius * 9 / 5) + 32) << endl;
 		cout << "Your temperature in Kelvin is " << (inputCelcius + 273.15) << endl;
 	}
 
 	else if (userInput == 'f') {
-		cout << "Enter the temperature in Farhenheit: \n";
-		cin >> inputFarhenheit;
-		cout << "Your temperature in Celcius is " << ((inputFarhenheit - 32) * 5 / 9) << endl;
-		cout << "Your temperature in Kelvin is " << ((inputFarhenheit - 32) * 5 / 9) + 273.15 << endl;
+		cout << "Enter the temperature in Fahrenheit: \n";
+		cin >> inputFahrenheit;
+		cout << "Your temperature in Celcius is " << ((inputFahrenheit - 32) * 5 / 9) << endl;
+		cout << "Your temperature in Kelvin is " << ((inputFahrenheit - 32) * 5 / 9) + 273.15 << endl;
 	}
 
 	else if (userInput == 'k') {
 		cout << "Enter the temperature in Kelvin: \n";
 		cin >> inputKelvin;
 		cout << "Your temperature in Celcius is " << (inputKelvin - 273.15) << endl;
-		cout << "Your temperature in Farhenheit is " << ((inputKelvin - 273.15) * 9 / 5 + 32) << endl;
+		cout << "Your temperature in Fahrenheit is " << ((inputKelvin - 273.15) * 9 / 5 + 32) << endl;
 	}
 
 	return 0;
